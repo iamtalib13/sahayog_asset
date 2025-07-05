80433cf15cbae3cb698716924393be7c57ae22a4
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-07-05 10:42:46 +0530

    fix: fix get_emp_details

--- sahayog_asset/sahayog_asset/doctype/asset_request/get_emp_details.py
+++ sahayog_asset/sahayog_asset/doctype/asset_request/get_emp_details.py
@@ -1,10 +1,22 @@
 import frappe
 
-
 @frappe.whitelist()
 def get_emp_details(emp_id):
-    return frappe.db.sql(
-        f"""select department,division,region,user_id,branch,district,employee_name,cell_number,designation,zone,reports_to
-        from `tabEmployee` where employee_id='{emp_id}';""",
-        as_dict=True,
+    return frappe.db.get_value(
+        "Employee",
+        {"employee_id": emp_id},
+        [
+            "department",
+            "custom_division",
+            "custom_region",
+            "user_id",
+            "branch",
+            "custom_district",
+            "employee_name",
+            "cell_number",
+            "designation",
+            "custom_zone",
+            "reports_to"
+        ],
+        as_dict=True
     )
