d0f8a20b6c519d948009ee68c2ef833de30129f9
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-07-05 11:14:55 +0530

    fix get_emp_details function by using ORM methods

--- sahayog_asset/sahayog_asset/doctype/asset_request/get_emp_details.py
+++ sahayog_asset/sahayog_asset/doctype/asset_request/get_emp_details.py
@@ -1,10 +1,23 @@
 import frappe
 
-
 @frappe.whitelist()
 def get_emp_details(emp_id):
-    return frappe.db.sql(
-        f"""select department,custom_division,custom_region,user_id,branch,custom_district,employee_name,cell_number,designation,custom_zone,reports_to
-        from `tabEmployee` where employee_id='{emp_id}';""",
-        as_dict=True,
+    employee = frappe.get_all(
+        "Employee",
+        filters={"employee_id": emp_id},
+        fields=[
+            "department",
+            "custom_division",
+            "custom_region",
+            "user_id",
+            "branch",
+            "custom_district",
+            "employee_name",
+            "cell_number",
+            "designation",
+            "custom_zone",
+            "reports_to",
+        ],
+        limit=1
     )
+    return employee
