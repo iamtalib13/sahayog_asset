2ebb75babd5462a2951e145e79919e4d0b28b14d
iamfaijank <iamfaijank@gmail.com>
2026-05-07 13:40:52 +0530

    feat: enhance Asset Dispatched Report - IT with filters and script logic

---
+++ sahayog_asset/sahayog_asset/report/asset_dispatched_report___it/asset_dispatched_report___it.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2026, Sid and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Asset Dispatched Report - IT"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date"
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date"
+		},
+		{
+			"fieldname": "status",
+			"label": __("Status"),
+			"fieldtype": "Select",
+			"options": "\nDraft\nPending\nRejected\nDispatched\nPartially Dispatched\nReceived\nPending From Purchase\nPending From Store Manager\nDelivered",
+			"default": ""
+		}
+	]
+};
